632347293ebd28aa34e737500780b03817612b8b
BhumikaKC <bhumikakc09@gmail.com>
2024-12-31 20:19:20 +0545

    added login page

--- MoneyMap/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MoneyMap.AssemblyInfo.cs
+++ MoneyMap/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MoneyMap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoneyMap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+de8ccb4e246d66c577599a0f77ce3b03e015fd92")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoneyMap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoneyMap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
