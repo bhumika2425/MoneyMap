615879a6f8c1ea701598d573d84fa024e9b9a9e3
BhumikaKC <bhumikakc09@gmail.com>
2025-01-02 21:17:09 +0545

    add transaction feature done

---
+++ MoneyMap/Components/Models/TransactionModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyMap.Components.Models
+{
+    public class TransactionModel
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public string Type { get; set; }
+        public decimal Amount { get; set; }
+        public string DebtSource { get; set; }
+        public DateTime? DueDate { get; set; }
+        public string Tags { get; set; }
+        public string Notes { get; set; }
+    }
+}
