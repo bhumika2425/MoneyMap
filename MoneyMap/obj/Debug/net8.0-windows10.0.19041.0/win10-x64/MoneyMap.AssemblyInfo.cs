d8c426f6edfb35eca92079ab12bc2f2fdff2d8b2
BhumikaKC <bhumikakc09@gmail.com>
2025-01-15 17:34:06 +0545

    Added all the required features for the project except dashboard filtering

--- MoneyMap/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MoneyMap.AssemblyInfo.cs
+++ MoneyMap/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MoneyMap.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoneyMap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b5ff546d9926de358fc39d6101b1901b06dbffba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+91e3ea290bf11179e7d7c6d720d4aef872b1a648")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoneyMap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoneyMap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
